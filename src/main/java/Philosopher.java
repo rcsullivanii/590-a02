ac780539d3ab9077c89dd7a1865d9357b154485d
Robert Sullivan <rsullivan@unc.edu>
2025-01-28 17:41:27 -0500

    Add Simulation Summary

--- src/main/java/Philosopher.java
+++ src/main/java/Philosopher.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import java.util.HashMap;
 // import Lock to control access to shared recourses (forks)
 // in our table class, we will create Philosophers with reentrant locks
 import java.util.concurrent.locks.Lock;
@@ -8,18 +9,21 @@ public class Philosopher extends Thread {
     private final int id;
     private final Lock leftFork;
     private final Lock rightFork;
+    private HashMap<Integer, Integer> eatingCount = new HashMap<>();
 
-    public Philosopher(int id, Lock leftFork, Lock rightFork) {
+    public Philosopher(int id, Lock leftFork, Lock rightFork, HashMap<Integer, Integer> eatingCount) {
         this.id = id;
         this.leftFork = leftFork;
         this.rightFork = rightFork;
+        // Pass a shared map in Table so we may record each time a Philosopher eats
+        this.eatingCount = eatingCount;
     }
 
     @Override 
     public void run() {
         try {
             // ideally we want to infinitely loop here until the thread is interrupted
-            while (true) {
+            while (!Thread.currentThread().isInterrupted()) {
                 think();
                 eat();
             }
@@ -31,23 +35,31 @@ public class Philosopher extends Thread {
 
     private void think() throws InterruptedException {
         System.out.println("Philosopher " + id + " is thinking");
-        Thread.sleep((int) Math.random() * 3000);
+        Thread.sleep((int) (Math.random() * 3000));
     }
 
     private void eat() throws InterruptedException {
-        // Lock both resources and log the action
+        // use try and finally to make sure that lock is released no matter what
         leftFork.lock();
-        System.out.println("Philosopher " + id + " acquired left fork");
-        rightFork.lock();
-        System.out.println("Philosopher " + id + " acquired right fork");
-
-        System.out.println("Philosopher " + id + " is eating");
-        Thread.sleep((int) Math.random() * 3000);
-
-        leftFork.unlock();
-        System.out.println("Philosopher " + id + " released left fork");
-        rightFork.unlock();
-        System.out.println("Philosopher " + id + " released right fork");
+        try {
+            System.out.println("Philosopher " + id + " acquired left fork");
+            rightFork.lock();
+            try {
+                System.out.println("Philosopher " + id + " acquired right fork");
+                System.out.println("Philosopher " + id + " is eating");
+                
+                synchronized (eatingCount) {
+                    eatingCount.put(id, eatingCount.getOrDefault(id, 0) + 1);
+                }
+                
+                Thread.sleep((int) (Math.random() * 3000));
+            } finally {
+                rightFork.unlock();
+                System.out.println("Philosopher " + id + " released right fork");
+            }
+        } finally {
+            leftFork.unlock();
+            System.out.println("Philosopher " + id + " released left fork");
+        }
     }
-
 }
--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.HashMap;
+import java.util.Map;
 // We import Reentrant lock to provide manual locking /  more control over resources
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -14,6 +16,8 @@ public class Table {
         // This gives us better control than just synchronized 
         ReentrantLock[] forks = new ReentrantLock[NUM_PHILOSOPHERS];
 
+        HashMap<Integer, Integer> eatingCount = new HashMap<>();
+
         for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
             // set fair paramter to true to prevent starvation (one philosopher continually being denied forks)
             forks[i] = new ReentrantLock(true);
@@ -25,9 +29,9 @@ public class Table {
 
             // By ensuring that the last philosopher picks up his right fork first, we can prevent potential deadlock
             if (i == NUM_PHILOSOPHERS - 1) {
-                philosophers[i] = new Philosopher(i, rightFork, leftFork);
+                philosophers[i] = new Philosopher(i, rightFork, leftFork, eatingCount);
             } else {
-                philosophers[i] = new Philosopher(i, leftFork, rightFork);
+                philosophers[i] = new Philosopher(i, leftFork, rightFork, eatingCount);
             }
 
             // This line will allocate a new thread for each philosopher (since Philosopher extends Thread and we override the run method)
@@ -35,7 +39,7 @@ public class Table {
         }
 
         try {
-            // Run for 10 seconds
+            // Run for 5 seconds
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -45,6 +49,24 @@ public class Table {
         // Close all threads
         for (Philosopher philosopher : philosophers) {
             philosopher.interrupt();
+        }
+
+        // join to wait for all threads to close
+        for (Philosopher philosopher : philosophers) {
+            try {
+                philosopher.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+
+        // Print eating count 
+        System.out.println("Simulation Complete | Eating Summary");
+        synchronized(eatingCount) {
+            for (Map.Entry<Integer, Integer> entry : eatingCount.entrySet()) {
+                System.out.println("Philosopher " + entry.getKey() + " ate " + entry.getValue() + " times");
+            }
         }
     }
 }
